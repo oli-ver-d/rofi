ed98f746757814da59061165515058dfd96d00aa
Oliver Dennis <oliver.d.333@proton.me>
2025-07-22 16:44:02 +0100

    Fixed the launcher path oops

--- packed/launcher.sh
+++ packed/launcher.sh
@@ -11,7 +11,7 @@
 ## style-6     style-7     style-8     style-9     style-10
 ## style-11    style-12    style-13    style-14    style-15
 
-dir="$HOME/Development/projects/rofi/packed"
+dir="$HOME/.config/rofi"
 theme='style-10'
 
 ## Run
